3ea80a326aa7bd76f39b821bbbd6bca4c7aad701
eli mateo <cr0wcallz@10-18-106-156.dynapool.wireless.nyu.edu>
2026-04-09 13:23:50 -0400

    Update DIGITAL_FINAL.ino

--- DIGITAL_FINAL.ino
+++ DIGITAL_FINAL.ino
@@ -6,17 +6,40 @@
 
 // GUItool: begin automatically generated code
 AudioSynthWaveform       waveform3; //xy=301,342
+AudioSynthWaveform       waveform5; //xy=301,446
+AudioSynthWaveform       waveform4; //xy=302,394
+AudioSynthWaveform       waveform6; //xy=302,497
+AudioSynthWaveform       waveform8; //xy=302,605
 AudioSynthWaveform       waveform2; //xy=305,284
 AudioSynthWaveform       waveform1;      //xy=306,226
-AudioMixer4              mixer1;         //xy=532,249
-AudioOutputI2S           i2s1;           //xy=743,206
+AudioSynthWaveform       waveform7; //xy=307,548
+AudioMixer4              mixer1;         //xy=584,307
+AudioMixer4              mixer2;         //xy=613,485
+AudioMixer4              mixer3;         //xy=771,362
+AudioOutputI2S           i2s1;           //xy=978,335
 AudioConnection          patchCord1(waveform3, 0, mixer1, 2);
-AudioConnection          patchCord2(waveform2, 0, mixer1, 1);
-AudioConnection          patchCord3(waveform1, 0, mixer1, 0);
-AudioConnection          patchCord4(mixer1, 0, i2s1, 0);
-AudioConnection          patchCord5(mixer1, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=609,384
+AudioConnection          patchCord2(waveform5, 0, mixer2, 0);
+AudioConnection          patchCord3(waveform4, 0, mixer1, 3);
+AudioConnection          patchCord4(waveform6, 0, mixer2, 1);
+AudioConnection          patchCord5(waveform8, 0, mixer2, 3);
+AudioConnection          patchCord6(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord7(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord8(waveform7, 0, mixer2, 2);
+AudioConnection          patchCord9(mixer1, 0, mixer3, 0);
+AudioConnection          patchCord10(mixer2, 0, mixer3, 1);
+AudioConnection          patchCord11(mixer3, 0, i2s1, 0);
+AudioConnection          patchCord12(mixer3, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=893,572
 // GUItool: end automatically generated code
+
+
+int intervalsMajorScale[8] = {0, 2, 4, 5, 7, 9, 11, 12};
+int intervalsLydianScale[8] = {0, 2, 4, 6, 7, 9, 11, 12};
+int intervalsMixoScale[8] = {0, 2, 4, 5, 7, 9, 10, 12};
+int intervalsDorianScale[8] = {0, 2, 3, 5, 7, 9, 10, 12};
+int intervalsMinorScale[8] = {0, 2, 3, 5, 7, 8, 10, 12};
+int intervalsPhrygianScale[8] = {0, 1, 3, 5, 7, 8, 10, 12};
+
 
 int gainControlMaster = A13;
 float gain = 0.0;
@@ -30,8 +53,11 @@ float freq3 = 392;
 int newRootNote = 0;
 int newThird = 0;
 int newFifth = 0;
+int newSeventh = 0;
 
 float semitonesUpOrDown = 0.0;
+
+int chooseScale = A12;
 
 int shift = 0;
 
@@ -49,17 +75,22 @@ AudioMemory(12);
   waveform1.amplitude(0.2);  //amplitude (volume) can be 0 to 1 and this is usually a good volume for headphones
   waveform1.frequency(freq1);
 
-  waveform2.begin(WAVEFORM_SINE);
-  waveform2.amplitude(0.2);  //amplitude (volume) can be 0 to 1 and this is usually a good volume for headphones
-  waveform2.frequency(freq2);
+  // waveform2.begin(WAVEFORM_SINE);
+  // waveform2.amplitude(0.2);  //amplitude (volume) can be 0 to 1 and this is usually a good volume for headphones
+  // waveform2.frequency(freq2);
 
-  waveform3.begin(WAVEFORM_SINE);
-  waveform3.amplitude(0.2);  //amplitude (volume) can be 0 to 1 and this is usually a good volume for headphones
-  waveform3.frequency(freq3);
+  // waveform3.begin(WAVEFORM_SINE);
+  // waveform3.amplitude(0.2);  //amplitude (volume) can be 0 to 1 and this is usually a good volume for headphones
+  // waveform3.frequency(freq3);
 
-  mixer1.gain(0,.2);
-  mixer1.gain(1,.2);
-  mixer1.gain(2,.2);
+  mixer1.gain(0,.1);
+  mixer1.gain(1,.1);
+  mixer1.gain(2,.1);
+  mixer1.gain(3,.1);
+  mixer2.gain(0,.1);
+  mixer2.gain(1,.1);
+  mixer2.gain(2,.1);
+  mixer2.gain(3,.1);
 
 }
 
@@ -72,19 +103,26 @@ void loop() {
   shift = map(analogRead(freqShiftAll), 0, 1023, 0, 500);
   // Serial.println(freq1, freq2, freq3);
 
-  newRootNote = freq1 * pow(2, semitonesUpOrDown/12.0);
-  newThird = freq1 * pow(2, (semitonesUpOrDown + 5)/12.0);
-  newFifth = freq1 * pow(2, (semitonesUpOrDown + 7)/12.0);
+  newRootNote = freq1 * pow(2, (intervalsDorianScale[(int)semitonesUpOrDown])/12.0);
+  newThird = freq1 * pow(2, (intervalsMajorScale[(int)semitonesUpOrDown] + 3)/12.0);
+  newFifth = freq1 * pow(2, (intervalsMajorScale[(int)semitonesUpOrDown] + 7)/12.0);
+  newSeventh = freq1 * pow(2, (intervalsMajorScale[(int)semitonesUpOrDown] + 11)/12.0);
 
   waveform1.frequency(newRootNote);
-  waveform2.frequency(newThird);
-  waveform3.frequency(newFifth);
+  // waveform2.frequency(newThird);
+  // waveform3.frequency(newFifth);
+  // waveform4.frequency(newSeventh);
+
   
   waveform1.begin(WAVEFORM_SINE);
-  waveform2.begin(WAVEFORM_SINE);
-  waveform3.begin(WAVEFORM_SINE);
+  // waveform2.begin(WAVEFORM_SINE);
+  // waveform3.begin(WAVEFORM_SINE);
+  // waveform4.begin(WAVEFORM_SINE);
 
   semitonesUpOrDown++;
+  if(semitonesUpOrDown >= 8) {
+    semitonesUpOrDown = 0;
+  }
   delay(500);
 
 
